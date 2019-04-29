66e14071936642a5076c86724211243b5a2a5478
D-Barradas <didier.barradas@gmail.com>
2019-04-29 11:02:57 +0300

    Update condensador_de_dataframes.R

--- condensador_de_dataframes.R
+++ condensador_de_dataframes.R
@@ -2,9 +2,9 @@ library(dplyr)
 library(reshape2)
 #library(ggplot2)
 #library(ggthemes)
-directorio_parent <- "/home/didier/Documentos/Proyecto_GO_cancer/GoIsingCancer/badukmovies-pro-collection"
+directorio_parent <- "/path/to/my/folder"
 setwd(directorio_parent)
-char <- "_tactics.txt"
+char <- "_tactics.txt" 
 for ( lista_de_folder_nivel1 in list.dirs(recursive = T,full.names = F)){
   #print (paste(directorio_parent,lista_de_folder_nivel1, sep = "/"))
   df_folders <- data.frame()
@@ -12,7 +12,7 @@ for ( lista_de_folder_nivel1 in list.dirs(recursive = T,full.names = F)){
   setwd(directorio_trabajo)
   for ( lista_de_files in list.files( )){
     if ( grepl(char,lista_de_files)){ ### equivalente a "char" in x
-       #print (lista_de_files)
+       #print (lista_de_files) ## see all the files in folder
        df_temp <- read.csv(lista_de_files,sep = "\t",header = T)
        df_folders <- merge(df_folders,df_temp,all=TRUE)
     }
